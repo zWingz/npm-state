afe7a2a431ab1b1d66a69c6070787345dd208bd9
zWing <371657110@qq.com>
2019-08-02 22:42:03 +0800

    feat: 🎸 init project

---
+++ docs/script/index.chunk.js
@@ -0,0 +1,551 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ 198:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(260);
+
+
+/***/ }),
+
+/***/ 209:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./af": 50,
+	"./af.js": 50,
+	"./ar": 51,
+	"./ar-dz": 52,
+	"./ar-dz.js": 52,
+	"./ar-kw": 53,
+	"./ar-kw.js": 53,
+	"./ar-ly": 54,
+	"./ar-ly.js": 54,
+	"./ar-ma": 55,
+	"./ar-ma.js": 55,
+	"./ar-sa": 56,
+	"./ar-sa.js": 56,
+	"./ar-tn": 57,
+	"./ar-tn.js": 57,
+	"./ar.js": 51,
+	"./az": 58,
+	"./az.js": 58,
+	"./be": 59,
+	"./be.js": 59,
+	"./bg": 60,
+	"./bg.js": 60,
+	"./bm": 61,
+	"./bm.js": 61,
+	"./bn": 62,
+	"./bn.js": 62,
+	"./bo": 63,
+	"./bo.js": 63,
+	"./br": 64,
+	"./br.js": 64,
+	"./bs": 65,
+	"./bs.js": 65,
+	"./ca": 66,
+	"./ca.js": 66,
+	"./cs": 67,
+	"./cs.js": 67,
+	"./cv": 68,
+	"./cv.js": 68,
+	"./cy": 69,
+	"./cy.js": 69,
+	"./da": 70,
+	"./da.js": 70,
+	"./de": 71,
+	"./de-at": 72,
+	"./de-at.js": 72,
+	"./de-ch": 73,
+	"./de-ch.js": 73,
+	"./de.js": 71,
+	"./dv": 74,
+	"./dv.js": 74,
+	"./el": 75,
+	"./el.js": 75,
+	"./en-SG": 76,
+	"./en-SG.js": 76,
+	"./en-au": 77,
+	"./en-au.js": 77,
+	"./en-ca": 78,
+	"./en-ca.js": 78,
+	"./en-gb": 79,
+	"./en-gb.js": 79,
+	"./en-ie": 80,
+	"./en-ie.js": 80,
+	"./en-il": 81,
+	"./en-il.js": 81,
+	"./en-nz": 82,
+	"./en-nz.js": 82,
+	"./eo": 83,
+	"./eo.js": 83,
+	"./es": 84,
+	"./es-do": 85,
+	"./es-do.js": 85,
+	"./es-us": 86,
+	"./es-us.js": 86,
+	"./es.js": 84,
+	"./et": 87,
+	"./et.js": 87,
+	"./eu": 88,
+	"./eu.js": 88,
+	"./fa": 89,
+	"./fa.js": 89,
+	"./fi": 90,
+	"./fi.js": 90,
+	"./fo": 91,
+	"./fo.js": 91,
+	"./fr": 92,
+	"./fr-ca": 93,
+	"./fr-ca.js": 93,
+	"./fr-ch": 94,
+	"./fr-ch.js": 94,
+	"./fr.js": 92,
+	"./fy": 95,
+	"./fy.js": 95,
+	"./ga": 96,
+	"./ga.js": 96,
+	"./gd": 97,
+	"./gd.js": 97,
+	"./gl": 98,
+	"./gl.js": 98,
+	"./gom-latn": 99,
+	"./gom-latn.js": 99,
+	"./gu": 100,
+	"./gu.js": 100,
+	"./he": 101,
+	"./he.js": 101,
+	"./hi": 102,
+	"./hi.js": 102,
+	"./hr": 103,
+	"./hr.js": 103,
+	"./hu": 104,
+	"./hu.js": 104,
+	"./hy-am": 105,
+	"./hy-am.js": 105,
+	"./id": 106,
+	"./id.js": 106,
+	"./is": 107,
+	"./is.js": 107,
+	"./it": 108,
+	"./it-ch": 109,
+	"./it-ch.js": 109,
+	"./it.js": 108,
+	"./ja": 110,
+	"./ja.js": 110,
+	"./jv": 111,
+	"./jv.js": 111,
+	"./ka": 112,
+	"./ka.js": 112,
+	"./kk": 113,
+	"./kk.js": 113,
+	"./km": 114,
+	"./km.js": 114,
+	"./kn": 115,
+	"./kn.js": 115,
+	"./ko": 116,
+	"./ko.js": 116,
+	"./ku": 117,
+	"./ku.js": 117,
+	"./ky": 118,
+	"./ky.js": 118,
+	"./lb": 119,
+	"./lb.js": 119,
+	"./lo": 120,
+	"./lo.js": 120,
+	"./lt": 121,
+	"./lt.js": 121,
+	"./lv": 122,
+	"./lv.js": 122,
+	"./me": 123,
+	"./me.js": 123,
+	"./mi": 124,
+	"./mi.js": 124,
+	"./mk": 125,
+	"./mk.js": 125,
+	"./ml": 126,
+	"./ml.js": 126,
+	"./mn": 127,
+	"./mn.js": 127,
+	"./mr": 128,
+	"./mr.js": 128,
+	"./ms": 129,
+	"./ms-my": 130,
+	"./ms-my.js": 130,
+	"./ms.js": 129,
+	"./mt": 131,
+	"./mt.js": 131,
+	"./my": 132,
+	"./my.js": 132,
+	"./nb": 133,
+	"./nb.js": 133,
+	"./ne": 134,
+	"./ne.js": 134,
+	"./nl": 135,
+	"./nl-be": 136,
+	"./nl-be.js": 136,
+	"./nl.js": 135,
+	"./nn": 137,
+	"./nn.js": 137,
+	"./pa-in": 138,
+	"./pa-in.js": 138,
+	"./pl": 139,
+	"./pl.js": 139,
+	"./pt": 140,
+	"./pt-br": 141,
+	"./pt-br.js": 141,
+	"./pt.js": 140,
+	"./ro": 142,
+	"./ro.js": 142,
+	"./ru": 143,
+	"./ru.js": 143,
+	"./sd": 144,
+	"./sd.js": 144,
+	"./se": 145,
+	"./se.js": 145,
+	"./si": 146,
+	"./si.js": 146,
+	"./sk": 147,
+	"./sk.js": 147,
+	"./sl": 148,
+	"./sl.js": 148,
+	"./sq": 149,
+	"./sq.js": 149,
+	"./sr": 150,
+	"./sr-cyrl": 151,
+	"./sr-cyrl.js": 151,
+	"./sr.js": 150,
+	"./ss": 152,
+	"./ss.js": 152,
+	"./sv": 153,
+	"./sv.js": 153,
+	"./sw": 154,
+	"./sw.js": 154,
+	"./ta": 155,
+	"./ta.js": 155,
+	"./te": 156,
+	"./te.js": 156,
+	"./tet": 157,
+	"./tet.js": 157,
+	"./tg": 158,
+	"./tg.js": 158,
+	"./th": 159,
+	"./th.js": 159,
+	"./tl-ph": 160,
+	"./tl-ph.js": 160,
+	"./tlh": 161,
+	"./tlh.js": 161,
+	"./tr": 162,
+	"./tr.js": 162,
+	"./tzl": 163,
+	"./tzl.js": 163,
+	"./tzm": 164,
+	"./tzm-latn": 165,
+	"./tzm-latn.js": 165,
+	"./tzm.js": 164,
+	"./ug-cn": 166,
+	"./ug-cn.js": 166,
+	"./uk": 167,
+	"./uk.js": 167,
+	"./ur": 168,
+	"./ur.js": 168,
+	"./uz": 169,
+	"./uz-latn": 170,
+	"./uz-latn.js": 170,
+	"./uz.js": 169,
+	"./vi": 171,
+	"./vi.js": 171,
+	"./x-pseudo": 172,
+	"./x-pseudo.js": 172,
+	"./yo": 173,
+	"./yo.js": 173,
+	"./zh-cn": 174,
+	"./zh-cn.js": 174,
+	"./zh-hk": 175,
+	"./zh-hk.js": 175,
+	"./zh-tw": 176,
+	"./zh-tw.js": 176
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = 209;
+
+/***/ }),
+
+/***/ 210:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 260:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/antd/es/date-picker/style/css.js + 4 modules
+var css = __webpack_require__(259);
+
+// EXTERNAL MODULE: ./node_modules/antd/es/date-picker/index.js + 98 modules
+var date_picker = __webpack_require__(196);
+
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(1);
+
+// EXTERNAL MODULE: ./node_modules/react-dom/index.js
+var react_dom = __webpack_require__(8);
+
+// EXTERNAL MODULE: ./node_modules/@antv/g2/build/g2.js
+var g2 = __webpack_require__(190);
+
+// CONCATENATED MODULE: ./src/http.ts
+const API_URL = 'https://api.npmjs.org';
+const REGISTRY_API = 'http://registry.npmjs.org';
+
+function parseUrl(url, params = {}) {
+  return url.replace(/(:\w+)/g, (match, $1) => {
+    return params[$1.slice(1)];
+  });
+}
+
+class Http {
+  fetch(url, options = {}) {
+    return fetch(url, Object.assign({}, options, {
+      method: options.method || 'GET'
+    })).then(r => r.json());
+  }
+
+  async getRange(pkg, range) {
+    const url = parseUrl('downloads/range/:range/:pkg', {
+      pkg,
+      range: `${range[0]}:${range[1]}`
+    });
+    const data = await this.fetch(`${API_URL}/${url}`);
+    return data;
+  }
+
+}
+
+const http = new Http();
+// CONCATENATED MODULE: ./src/npmCount.tsx
+// https://github.com/npm/registry/blob/master/docs/download-counts.md
+
+
+
+class npmCount_NpmCount extends react["PureComponent"] {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      downloads: {
+        downloads: [],
+        start: '',
+        end: '',
+        package: ''
+      },
+      weekType: false
+    };
+    this.chart = null;
+    this.$el = react["createRef"]();
+  }
+
+  async getDownloadCount() {
+    const data = await http.getRange(this.props.repo, this.props.range);
+    let {
+      downloads
+    } = data;
+    const {
+      length
+    } = downloads;
+    let weekType = false;
+
+    if (length > 60) {
+      weekType = true;
+      const tmp = [];
+      const step = 7;
+
+      for (let i = length - 1; i > 0;) {
+        const start = i - step;
+        const arr = downloads.slice(Math.max(0, start), Math.max(0, i));
+        if (!arr.length) return;
+        tmp.unshift({
+          day: `${arr[0].day}/${arr[arr.length - 1].day}`,
+          downloads: arr.reduce((sum, each) => sum + each.downloads, 0)
+        });
+        i = start;
+      }
+
+      downloads = tmp;
+    }
+
+    this.setState({
+      downloads: Object.assign({}, data, {
+        downloads
+      }),
+      weekType
+    }, () => {
+      this.renderChart();
+    });
+  }
+
+  async componentDidMount() {
+    this.getDownloadCount();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {
+      repo,
+      range
+    } = this.props;
+
+    if (prevProps.repo !== repo || prevProps.range !== range) {
+      this.getDownloadCount();
+    }
+  }
+
+  renderChart() {
+    const {
+      weekType
+    } = this.state;
+    let chart;
+
+    if (!this.chart) {
+      this.chart = new g2["Chart"]({
+        container: this.$el.current,
+        forceFit: true,
+        height: 200,
+        padding: 'auto'
+      });
+      chart = this.chart;
+      chart.scale('downloads', {
+        alias: 'download count'
+      });
+      chart.scale('day', {
+        alias: 'day' // type: 'time'
+
+      });
+      chart.tooltip({
+        crosshairs: {
+          type: 'line'
+        }
+      });
+      chart.line().position('day*downloads').style({
+        stroke: 'rgb(200,0,4)'
+      });
+      chart.source(this.state.downloads.downloads);
+      this.chart.render();
+    } else {
+      this.chart.axis('day', {
+        label: weekType ? null : {}
+      });
+      this.chart.changeData(this.state.downloads.downloads);
+    }
+  }
+
+  render() {
+    const {
+      repo
+    } = this.props;
+    return react["createElement"]("div", {
+      className: 'chart'
+    }, react["createElement"]("div", {
+      className: 'repo-title'
+    }, react["createElement"]("a", {
+      target: '_blank',
+      href: `https://www.npmjs.com/package/${repo}`
+    }, repo)), react["createElement"]("div", {
+      className: 'g2-chart',
+      ref: this.$el
+    }));
+  }
+
+}
+// EXTERNAL MODULE: ./node_modules/moment/moment.js
+var moment = __webpack_require__(2);
+
+// EXTERNAL MODULE: ./src/style.less
+var style = __webpack_require__(210);
+
+// CONCATENATED MODULE: ./src/index.tsx
+
+
+
+
+ // import Select from './component/Select'
+
+
+
+const {
+  RangePicker
+} = date_picker["a" /* default */];
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current > moment().endOf('day');
+}
+
+const start = moment().subtract(7, 'days');
+const end = moment();
+const FORMAT = 'YYYY-MM-DD';
+
+function App() {
+  const [value, setRange] = react["useState"]({
+    range: [start, end],
+    format: [start.format(FORMAT), end.format(FORMAT)]
+  });
+  const onChange = react["useCallback"]((arg1, arg2) => {
+    setRange({
+      range: arg1,
+      format: arg2
+    });
+  }, []);
+  return react["createElement"]("div", {
+    className: 'app-container'
+  }, react["createElement"]("h2", {
+    className: 'app-title'
+  }, react["createElement"]("svg", {
+    viewBox: '0 0 780 250'
+  }, react["createElement"]("path", {
+    fill: '#231F20',
+    d: 'M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z'
+  })), react["createElement"]("span", {
+    style: {
+      marginRight: 'auto'
+    }
+  }, "Downloads Count"), react["createElement"](RangePicker, {
+    disabledDate: disabledDate,
+    value: value.range,
+    onChange: onChange
+  })), react["createElement"]("div", {
+    className: 'chart-wrapper'
+  }, NPM_REPO.map(each => react["createElement"](npmCount_NpmCount, {
+    key: each,
+    repo: each,
+    range: value.format
+  }))));
+}
+
+react_dom["render"](react["createElement"](App, null), document.getElementById('app'));
+
+/***/ })
+
+},[[198,2,0]]]);
